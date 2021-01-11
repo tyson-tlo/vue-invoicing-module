38829ff8aecba402e92f49a4cccae2d069927938
Tyson London <tyson@modcul.com>
2021-01-11 14:41:53 +0100

    first commit

--- src/views/invoices/ViewInvoice.vue
+++ src/views/invoices/ViewInvoice.vue
@@ -1,29 +1,41 @@
 <template>
     <div class="invoice-container">
-       <div class="title-container" v-if="invoice">
-           <div v-if="invoice.customer">
-               <h3>{{ invoice.customer.name }}</h3>
-               <h4>{{ invoice.customer.address }}</h4>
-               <h4>{{ invoice.customer.city }}</h4>
-               <h4>{{ invoice.customer.province }}</h4>
-               <h4>{{ invoice.customer.postal_code }}</h4>
-               <h4>{{ invoice.customer.country }}</h4>
-           </div>
-           <div>
-               <h3>Company Business</h3>
-               <h4>123 Business Street</h4>
-               <h4>Edmonton</h4>
-           </div>
-       </div>
+        <div class="title-container" v-if="invoice">
+            <div v-if="invoice.customer">
+                <h3>{{ invoice.customer.name }}</h3>
+                <h4>{{ invoice.customer.address }}</h4>
+                <h4>{{ invoice.customer.city }}</h4>
+                <h4>{{ invoice.customer.province }}</h4>
+                <h4>{{ invoice.customer.postal_code }}</h4>
+                <h4>{{ invoice.customer.country }}</h4>
+            </div>
+            <div class="company-details">
+                <h3>Company Business</h3>
+                <h4>123 Business Street</h4>
+                <h4>Edmonton, AB</h4>
+                <h4>T5R-4E3</h4>
+                <h4>Canada</h4>
+            </div>
+        </div>
+
+        <hr>
+        <div v-if="invoices">
+            <invoice-item-row v-for="[item, i] in invoices.items" :key="i" :item="item"></invoice-item-row>
+        </div>        
     </div>
 </template>
 
 <script>
 import invoices from '../../data/invoices'
 
+import InvoiceItemRow from './partials/InvoiceItemRow.vue'
+
 export default {
     mounted() {
         this.getInvoice()
+    },
+    components: {
+        InvoiceItemRow
     },
     data () {
         return {
@@ -63,8 +75,8 @@ export default {
             text-align: left;
         }
 
-        /* .title-container:last-child {
+        .title-container .company-details  {
             text-align: right
-        } */
+        }
     }
 </style>
---
+++ src/views/invoices/partials/InvoiceItemRow.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{ rowTotal.toLocaleString() }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['item'],
+    computed: {
+        rowTotal() {
+            return (this.item.unit_cost * this.item.units).toLocalString()
+        }
+    }
+}
+</script>
